b57518418d6d434743b516b5e087cd51b61e24d5
Marwilpuk <33729831+Marwilpuk@users.noreply.github.com>
2021-06-30 21:50:07 +0200

    starting point

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,15 @@
+const homePageImageStyle = {
+  display: "block",
+  marginleft: "auto",
+  marginright: "auto",
+  width: "100%"
+};
+
+
+export const App = () => {
+  return (
+    <div>
+      <img src={"/assets/uS.jpeg"} style={homePageImageStyle} />
+    </div>
+  );
+}
