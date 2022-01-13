da1d796319644a7dc0cb0e9293013b83ff22b783
Marko Todorovic <todorovic.m@outlook.com>
2022-01-13 20:43:19 +0100

    #211-add global style for fullscreen messages

--- src/app/layout/NotFound.tsx
+++ src/app/layout/NotFound.tsx
@@ -1,80 +1,32 @@
-import { Button, Grid, GridColumn, Image } from "semantic-ui-react";
+import { Button, Grid, GridColumn, Header, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import { EkvitiColors } from "./EkvitiColors";
+import { styles } from "../../app/layout/FullScreenCardStyle";
 
 const NotFound = () => {
   return (
-      <Grid style={GridStyle} verticalAlign='middle' columns={4} centered>
-        <GridColumn mobile={14} style={ColumnStyle} textAlign="center">
-            <Image src="/assets/LogoForNotFound.png" centered style={LogoStyle}></Image> 
-            <Image src="/assets/KnightConfused.png" centered></Image> 
-            <span className="ekvitiPrimaryFont" style={OopsStyle}>Oops!</span>
-            <br/>
-            <span className="ekvitiPrimaryFont" style={TextStyle}>Sve smo pretražili, ali ovu putanju nismo pronašli.</span>
-            <br/>
-            <Button
-              as={Link} to="/"
-              className="ekvitiPrimaryFont"
-              content="Vrati se na početnu"
-              style={ButtonStyle}
-              />
-        </GridColumn>
-      </Grid>
+    <Grid style={styles.GridStyle} verticalAlign="middle" columns={4} centered>
+      <GridColumn mobile={14} style={styles.ColumnStyle} textAlign="center">
+        <Image
+          src="/assets/LogoForNotFound.png"
+          centered
+          style={styles.LogoStyle}
+        ></Image>
+        <Image src="/assets/KnightConfused.png" centered></Image>
+        <Header as="h2" content="Oops!" />
+        <br />
+        <p style={styles.TextStyle}>
+          Sve smo pretražili, ali ovu putanju nismo pronašli.
+        </p>
+        <br />
+        <Button
+          as={Link}
+          to="/"
+          content="Vrati se na početnu"
+          style={styles.ButtonStyle}
+        />
+      </GridColumn>
+    </Grid>
   );
 };
 
 export default NotFound;
-
-const ColumnStyle = {
-  minHeight: "62%",
-  backgroundColor: EkvitiColors.white,
-  borderRadius: "10px",
-  maxWidth: "650px"
-};
-
-const GridStyle = {
-  backgroundColor: EkvitiColors.secondary,
-  height: "100%",
-  width: "100%",
-  marginTop: "0px",
-  marginLeft: "0px",
-  position: "fixed",
-  top: "0",
-  left: "0"
-};
-
-const LogoStyle = {
-  paddingTop: "10%",
-  paddingBottom: "4%",
-  height: "auto",
-  width: "57.5%"
-};
-
-const OopsStyle = {
-  paddingTop: "5%",
-  paddingBottom: "3%",
-  display: "inline-block",
-  fontSize: 25
-};
-
-const ButtonStyle = {
-  marginRight: "0em", 
-  marginTop: "7%",
-  minWidth: "77%",
-  display: "inline-block",
-  fontSize: "19px",
-  paddingTop: "23px",
-  paddingBottom: "23px",
-  borderRadius: "10px",
-  boxShadow: `00 8px 10px -6px ${EkvitiColors.primary}`,
-  backgroundColor: EkvitiColors.primary, 
-  color: "white",
-  marginBottom: "5%"
-};
-
-const TextStyle = {
-  display: "inline-block", 
-  fontSize: 19, 
-  color: EkvitiColors.primary,
-  letterSpacing: "0.6px"
-};
--- src/features/user/RegisterSuccess.tsx
+++ src/features/user/RegisterSuccess.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
 import queryString from "query-string";
-import { Button, Card, Container, Grid, GridColumn, Header, Icon, Image, Segment } from "semantic-ui-react";
+import { Button, Grid, Header, Image } from "semantic-ui-react";
 import agent from "../../app/api/agent";
 import { toast } from "react-toastify";
-import { EkvitiColors } from "../../app/layout/EkvitiColors";
+import { styles } from "../../app/layout/FullScreenCardStyle";
 
 const RegisterSuccess: React.FC<RouteComponentProps> = ({ location }) => {
   const { email } = queryString.parse(location.search);
@@ -18,25 +18,31 @@ const RegisterSuccess: React.FC<RouteComponentProps> = ({ location }) => {
   };
 
   return (
-    <Grid style={GridStyle} verticalAlign="middle" centered>
-      <Grid.Column mobile={14} style={ColumnStyle} textAlign="center">
+    <Grid style={styles.GridStyle} verticalAlign="middle" centered>
+      <Grid.Column mobile={14} style={styles.ColumnStyle} textAlign="center">
         <Image
           src="/assets/LogInEkvitiLogo.png"
-          style={LogoStyle}
+          style={styles.LogoStyle}
           centered
         />
-        <Image
-          src="/assets/KnightSuccessfullRegistration.png"
-          centered
-        />
-        <Header as="h2" style={HeaderStyle}  content="Uspešna registracija!"/>
-        <Container style={TextStyle} >
-          Uspešno ste se registrovali, potvrdite putem linka koji smo poslali na
-          vašu mejl adresu.
-        </Container>
-        <Container style={{...TextStyle, marginTop:"40px", marginBottom:"20px"}}>Niste dobili verifikacioni mejl?</Container>
+        <Image src="/assets/KnightSuccessfullRegistration.png" centered />
+        <Header as="h2" content="Uspešna registracija!" />
+        <p style={styles.TextStyle}>
+          Uspešno ste se registrovali, potvrdite putem linka koji
+          <br />
+          smo poslali na vašu mejl adresu.
+        </p>
+        <p
+          style={{
+            ...styles.TextStyle,
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+        >
+          Niste dobili verifikacioni mejl?
+        </p>
         <Button
-          style={ButtonStyle}
+          style={styles.ButtonStyle}
           onClick={handleConfirmEmailResend}
           content="Ponovo pošalji potvrdu"
         />
@@ -46,62 +52,3 @@ const RegisterSuccess: React.FC<RouteComponentProps> = ({ location }) => {
 };
 
 export default RegisterSuccess;
-
-const GridStyle = {
-  backgroundColor: EkvitiColors.secondary,
-  height: "100%",
-  width: "100%",
-  marginTop: "0px",
-  marginLeft: "0px",
-  position: "fixed",
-  top: "0",
-  left: "0"
-};
-
-const ColumnStyle = {
-  minHeight: "62%",
-  backgroundColor: EkvitiColors.white,
-  borderRadius: "10px",
-  maxWidth: "650px"
-};
-
-const LogoStyle = {
-  paddingTop: "10%",
-  paddingBottom: "4%",
-  height: "auto",
-  width: "57.5%"
-};
-
-const ButtonStyle = {
-  marginRight: "0em", 
-  //marginTop: "7%",
-  minWidth: "77%",
-  //display: "inline-block",
-  fontSize: "19px",
-  paddingTop: "23px",
-  paddingBottom: "23px",
-  borderRadius: "10px",
-  boxShadow: `00 8px 10px -6px ${EkvitiColors.primary}`,
-  backgroundColor: EkvitiColors.primary, 
-  color: "white",
-  marginBottom: "5%",
-  //fontWeight: 100
-};
-
-const HeaderStyle = {
-  //fontWeight: " !important"
-  //paddingTop: "5%",
-  //paddingBottom: "3%",
-  //display: "inline-block",
-  //fontSize: 25
-  //color: EkvitiColors.primary
-  //textShadow: "2px 2px black"
-};
-
-const TextStyle = {
-  fontSize: 19, 
-  color: EkvitiColors.primary,
-  //letterSpacing: "0.6px",
-  //fontWeight: "bold"
-};
-
