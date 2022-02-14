b786a3fc4125293f84b097affd3b37e2b0ad5b4c
Marko Todorovic <todorovic.m@outlook.com>
2022-02-14 21:31:29 +0100

    set header content insted of child

--- src/features/profile/ProfileHeader.tsx
+++ src/features/profile/ProfileHeader.tsx
@@ -1,17 +1,13 @@
 import { observer } from 'mobx-react-lite';
+import { useContext } from 'react';
 
 import { Segment, Item, Header, Grid, Statistic } from 'semantic-ui-react';
+import { RootStoreContext } from '../../app/stores/rootStore';
 
 
 const ProfileHeader = () => {
-  // const rootStore = useContext(RootStoreContext);
-  //   const {user} = rootStore.userStore;
-
-  // return (
-  //     <Segment>
-  //       test
-  //     </Segment>
-  // )
+  const rootStore = useContext(RootStoreContext);
+  const { user } = rootStore.userStore;
 
   return (
     <Segment>
@@ -19,24 +15,18 @@ const ProfileHeader = () => {
         <Grid.Column width={12}>
           <Item.Group>
             <Item>
-              <Item.Image
-                avatar
-                size='small'
-                src={'/assets/user.png'}
-              />
-              <Item.Content verticalAlign='middle'>
-                {/* <Header as='h1'>{user?.username}</Header> */}
-                <Header as='h1'>test</Header>
+              <Item.Image avatar size="small" src={"/assets/user.png"} />
+              <Item.Content verticalAlign="middle">
+                <Header as="h1" content={user?.username}>
+                </Header>
               </Item.Content>
             </Item>
           </Item.Group>
         </Grid.Column>
         <Grid.Column width={4}>
           <Statistic.Group widths={2}>
-            {/* <Statistic label='Xp' value={user?.currentXp}/>
-            <Statistic label='Level' value={user?.currentLevel}/> */}
-            <Statistic label='Xp' value={5}/>
-            <Statistic label='Level' value={5}/>
+            <Statistic label="Xp" value={user?.currentXp} />
+            <Statistic label="Level" value={user?.currentLevel} />
           </Statistic.Group>
         </Grid.Column>
       </Grid>
