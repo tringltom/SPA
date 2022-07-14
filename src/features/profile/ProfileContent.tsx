3af7840f5ea977cb4ff3b83f182b318452a4bdde
mushmulla <jevgenije@hotmail.com>
2022-07-14 20:52:59 +0200

    comments resolved.

--- src/features/profile/ProfileContent.tsx
+++ src/features/profile/ProfileContent.tsx
@@ -13,17 +13,14 @@ interface IProps {
 };
 
 const ProfileContent: React.FC<IProps> = ({ userId, isProfileOwner }) => {
-    const rootStore = useContext(RootStoreContext);
-    
-
-  
+   
     
     const panes = [
-        {menuItem: 'Detalji', render: () => <Tab.Pane><ProfileAbout isProfileOwner={isProfileOwner}/></Tab.Pane>},
-        {menuItem: 'Veštine', render: () => <Tab.Pane><ProfileSkills userId={userId} isProfileOwner={isProfileOwner}/></Tab.Pane>},
+        {menuItem: 'Detalji', render: () => <Tab.Pane><ProfileAbout isProfileOwner/></Tab.Pane>},
+        {menuItem: 'Veštine', render: () => <Tab.Pane><ProfileSkills userId={userId} isProfileOwner/></Tab.Pane>},
         {menuItem: 'Aktivnosti na čekanju', render: () =><Tab.Pane active={Boolean(isProfileOwner)}><ProfilePendingActivities/></Tab.Pane>},
         {menuItem: 'Odobrene aktivnosti', render: () => <Tab.Pane><ProfileApprovedActivities userId={userId}/></Tab.Pane>},
-        {menuItem: 'Omiljene aktivnosti', render: () => <Tab.Pane><ProfileFavoriteActivities userId={userId} isProfileOwner={isProfileOwner} /></Tab.Pane>},
+        {menuItem: 'Omiljene aktivnosti', render: () => <Tab.Pane><ProfileFavoriteActivities userId={userId} isProfileOwner/></Tab.Pane>},
     ]
 
     return (
